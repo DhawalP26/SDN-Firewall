b628a1b159afc203f78bc374d244c800b14d8dbd
Dhawal Pathak <your-email@example.com>
2026-04-16 13:34:45 +0000

    Clean SDN Firewall Project

---
+++ firewall.py
@@ -0,0 +1,29 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.packet import ipv4
+
+log = core.getLogger()
+
+def _handle_PacketIn(event):
+    packet = event.parsed
+
+    ip_packet = packet.find('ipv4')
+
+    if ip_packet:
+        src = str(ip_packet.srcip)
+        dst = str(ip_packet.dstip)
+
+        # 🚫 BLOCK: h1 → h3
+        if src == "10.0.0.1" and dst == "10.0.0.3":
+            log.info("Blocked %s -> %s", src, dst)
+            return
+
+    # ✅ ALLOW (flood)
+    msg = of.ofp_packet_out()
+    msg.data = event.ofp
+    msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+    event.connection.send(msg)
+
+def launch():
+    core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
+    log.info("Firewall Loaded")
